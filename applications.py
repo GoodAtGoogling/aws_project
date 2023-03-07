b50ba3ee490c8f155ab2738f5f64cdb1fd60bfd8
GoodAtGoogling <44258971+GoodAtGoogling@users.noreply.github.com>
2023-03-07 12:14:45 +0100

    update

--- applications.py
+++ applications.py
@@ -1,9 +1,7 @@
 
-import requests
-import sql_lite_db
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS
-
+import sql_lite_db
 
 
 application = Flask(__name__)
@@ -14,9 +12,28 @@ def index():
     employees = sql_lite_db.list_employees()
     return employees
 
-@application.route("/employees/add")
+@application.route("/employees/add", methods=['GET','POST'])
 def add(): 
-    sql_lite_db.add_employee(firstName,lastName,emailId)
+    if request.method=="POST":
+        firstName = request.form["fistName"]
+        lastName = request.form["lastName"]
+        emailId = request.form["emailId"]
+        sql_lite_db.add_employee(firstName,lastName,emailId)
+
+@application.route("/employee/delete/<int:id>", methods =['GET','POST'])
+def delete(id):
+    sql_lite_db.delete_employee(id)
+
+@application.route("/employee/update/<int:id>", methods =['GET','POST'])
+def update(id):
+        if request.method=="POST":
+            firstName = request.form["fistName"]
+            lastName = request.form["lastName"]
+            emailId = request.form["emailId"]
+            sql_lite_db.update_employee(id,firstName, lastName, emailId)
+
+
+
 if __name__=="__main__":
     sql_lite_db.create_employee_table()
     application.run()
