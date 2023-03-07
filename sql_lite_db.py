2bb482109591aff10aa8eb32f6934d679c9b25d4
GoodAtGoogling <44258971+GoodAtGoogling@users.noreply.github.com>
2023-03-07 10:24:14 +0100

    typo

--- sql_lite_db.py
+++ sql_lite_db.py
@@ -1,11 +1,11 @@
 import sqlite3
  
-def get_database_connextion():
+def get_database_connexion():
     con = sqlite3.connect("employee.db")
     return con
  
 def create_employee_table():
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ CREATE TABLE IF NOT EXISTS employees(
                 id INTEGER PRIMARY KEY AUTOINCREMENT
@@ -20,7 +20,7 @@ def create_employee_table():
     con.close()
  
 def list_employees():
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ SELECT id, firstName, lastName, emailId
                 FROM employees
@@ -33,7 +33,7 @@ def list_employees():
     return result
  
 def load_employee(employee_id):
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ SELECT id, firstName, lastName, emailId
                 FROM employees
@@ -46,7 +46,7 @@ def load_employee(employee_id):
     return result
  
 def add_employee(firstName, lastName, emailId):
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ INSERT INTO employees(firstName, lastName, emailId)
                 VALUES(?, ?, ?);
@@ -57,7 +57,7 @@ def add_employee(firstName, lastName, emailId):
     con.close()
  
 def delete_employee(employee_id):
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ DELETE
                 FROM employees
@@ -69,7 +69,7 @@ def delete_employee(employee_id):
     con.close()
  
 def update_employee(employee_id, firstName, lastName,emailId):
-    con = get_database_connextion()
+    con = get_database_connexion()
     cursor = con.cursor()
     query = """ UPDATE employees
                 SET firstName = ? ,lastName = ? , emailId = ?
